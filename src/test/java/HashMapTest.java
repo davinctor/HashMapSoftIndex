2eb6c1f07c876f26373f29b858e73ece9f40f623
davinctor <victor.ponomarenko.o@gmail.com>
2015-03-20 15:49:43 +0300

    Changed test - test code divided into modules

--- src/test/java/HashMapTest.java
+++ src/test/java/HashMapTest.java
@@ -1,5 +1,6 @@
 
 import com.hashmap.OpenAddressingHashMap;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -10,61 +11,72 @@ import static org.junit.Assert.*;
 
 public class HashMapTest {
 
+    Map<Long, Integer> mapOA;   // open addressing
+    Map<Long, Integer> mapJU;   // java.util
+
+    @Before
+    public void beforeTest() {
+        mapOA = new OpenAddressingHashMap<Long, Integer>();
+    }
+
     @Test
-    public void basicTest() {
-        Map<Long, Integer> map = new OpenAddressingHashMap<Long, Integer>();
+    public void sizeTest() {
+        assertTrue(mapOA.isEmpty());
+        assertEquals(0, mapOA.size());
 
-        assertTrue(map.isEmpty());
-        assertEquals(0, map.size());
+        assertNotNull(mapOA.keySet());
+        assertNotNull(mapOA.values());
 
-        assertNotNull(map.keySet());
-        assertNotNull(map.values());
+        assertEquals(0, mapOA.keySet().size());
+        assertEquals(0, mapOA.values().size());
 
-        assertEquals(0, map.keySet().size());
-        assertEquals(0, map.values().size());
+        mapOA.put(1L, 15);
+        mapOA.put(2L, 30);
+        assertEquals(2, mapOA.size());
+        assertEquals(2, mapOA.keySet().size());
+        assertEquals(2, mapOA.values().size());
+    }
 
-
-        assertNotNull(map.put(1L, 15));
-        assertEquals(1, map.size());
-
-        assertFalse(map.containsKey(0.75f));
-        assertTrue(map.containsKey(1L));
-
-        map.put(2L, 30);
-        assertEquals(2, map.size());
-        // Because map.remove() wait for Object and "2" cast by default to Integer
-        // that's why hash code different and Object by this key doesn't exists in map
-        assertNotEquals(Integer.valueOf(30), map.remove(2));
-        assertEquals(Integer.valueOf(30), map.remove(2L));
-
-        map.put(3L, 45);
-        map.put(4L, 60);
-        assertEquals(Integer.valueOf(45), map.get(3L));
+    @Test
+    public void putGetTest() {
+        mapOA.put(3L, 45);
+        mapOA.put(4L, 60);
+        assertEquals(Integer.valueOf(45), mapOA.get(3L));
         // Because map.get() wait for Object and "4" cast by default to Integer
         // that's why hash code different and Object by this key doesn't exists in map
-        assertNotEquals(Integer.valueOf(60), map.get(4));
+        assertNotEquals(Integer.valueOf(60), mapOA.get(4));
+    }
 
-        Iterator<Long> iter = map.keySet().iterator();
+    @Test
+    public void containsTest() {
+        mapOA.put(1L, 45);
+        mapOA.put(2L, 60);
+        mapOA.put(3L, 75);
+        assertFalse(mapOA.containsKey(0.75f));
+        assertTrue(mapOA.containsKey(1L));
+        assertTrue(mapOA.containsValue(45));
+
+        Iterator<Long> iter = mapOA.keySet().iterator();
         int count = 0;
         while (iter.hasNext()) {
             Long key = iter.next();
-            if (!map.containsKey(key))
+            if (!mapOA.containsKey(key))
                 fail("Key(Long) getting from map("+key+") check failed by containsValue()");
             count++;
         }
         assertEquals(3, count);
 
-        Iterator<Integer> iter2 = map.values().iterator();
+        Iterator<Integer> iter2 = mapOA.values().iterator();
         count = 0;
         while (iter2.hasNext()) {
             Integer value = iter2.next();
-            if (!map.containsValue(value))
+            if (!mapOA.containsValue(value))
                 fail("Value(Integer) getting from map(" + value +") check failed by values()");
             count++;
         }
         assertEquals(count, 3);
 
-        Iterator<Map.Entry<Long,Integer>> iter3 = map.entrySet().iterator();
+        Iterator<Map.Entry<Long,Integer>> iter3 = mapOA.entrySet().iterator();
         Map.Entry<Long,Integer> entry;
         while (iter3.hasNext()) {
             entry = iter3.next();
@@ -72,21 +84,30 @@ public class HashMapTest {
             assertNotNull(entry.getValue());
             iter3.remove();
         }
-        assertEquals(0, map.size());
+        assertEquals(0, mapOA.size());
+    }
 
-        map.put(3L, 45);
-        map.put(4L, 60);
-        map.clear();
-        assertEquals(0, map.size());
+    @Test
+    public void clearTest() {
+        mapOA.put(3L, 45);
+        mapOA.put(4L, 60);
+        mapOA.clear();
+        assertEquals(0, mapOA.size());
+    }
 
-        map.put(Long.MAX_VALUE, Integer.MAX_VALUE);
-        assertEquals(null, map.get(Long.MAX_VALUE - 1L));
+    @Test
+    public void removeTest() {
+        mapOA.put(2L, 45);
+
+        // Because map.remove() wait for Object and "2" cast by default to Integer
+        // that's why hash code different and Object by this key doesn't exists in map
+        assertNotEquals(Integer.valueOf(45), mapOA.remove(2));
+        assertEquals(Integer.valueOf(45), mapOA.remove(2L));
     }
 
     @Test
     public void constructorTests() {
-        Map map = new OpenAddressingHashMap<Integer, Integer>();
-        assertEquals(0, map.size());
+        assertEquals(0, mapOA.size());
     }
 
     /**
@@ -99,16 +120,16 @@ public class HashMapTest {
     }
     **/
 
-    private static int LOAD_COUNT = 5000000;
+    private static int LOAD_COUNT = 50000;
 
 
-    private void hashMapSpeedTest(Map<Integer, Integer> map, boolean isPut) {
+    private void hashMapSpeedTest(Map<Long, Integer> map, boolean isPut) {
         long t0 = System.currentTimeMillis();
         for (int i = 0; i < LOAD_COUNT; i++) {
             if (isPut)
-                map.put(Integer.valueOf(i), Integer.valueOf(i));
+                map.put(Long.valueOf(i), Integer.valueOf(i));
             else
-                map.get(Integer.valueOf(i));
+                map.get(Long.valueOf(i));
         }
         long t1 = System.currentTimeMillis();
         System.out.println(map.getClass() + (isPut ? ".put() " : ".get() ")
@@ -118,56 +139,54 @@ public class HashMapTest {
 
     @Test
     public void hashMapSpeedTest() {
-        Map<Integer, Integer> mapO = new OpenAddressingHashMap<Integer, Integer>();
-        Map<Integer,Integer> mapH = new HashMap<Integer, Integer>();
-        hashMapSpeedTest(mapO, true);
-        hashMapSpeedTest(mapH, true);
-        hashMapSpeedTest(mapO, false);
-        hashMapSpeedTest(mapH, false);
+        mapJU = new HashMap<Long, Integer>();
+        hashMapSpeedTest(mapOA, true);
+        hashMapSpeedTest(mapJU, true);
+        hashMapSpeedTest(mapOA, false);
+        hashMapSpeedTest(mapJU, false);
     }
 
     @Test
     public void iteratorTest() {
-        OpenAddressingHashMap<Integer, Long> map = new OpenAddressingHashMap<Integer, Long>();
-        map.put(150, 400L);
-        map.put(300, 700L);
-        map.put(600, 1300L);
+        mapOA.put(150L, 400);
+        mapOA.put(300L, 700);
+        mapOA.put(600L, 1300);
 
-        Iterator<Integer> iter = map.keySet().iterator();
-        Integer key;
+        Iterator<Long> iter = mapOA.keySet().iterator();
+        Long key;
         while (iter.hasNext()) {
             key = iter.next();
-            assertNotNull(map.get(key));
+            assertNotNull(mapOA.get(key));
         }
     }
 
     @Test
     public void equalsTest() {
-        OpenAddressingHashMap<Integer, Long> map1 = new OpenAddressingHashMap<Integer, Long>();
-        map1.put(150, 400L);
-        map1.put(300, 700L);
-        map1.put(600, 1300L);
+        mapJU = new HashMap<Long, Integer>();
 
-        OpenAddressingHashMap<Integer, Long> map2 = new OpenAddressingHashMap<Integer, Long>();
-        map2.put(150, 400L);
-        map2.put(300, 700L);
-        map2.put(600, 1300L);
+        mapOA.put(150L, 400);
+        mapOA.put(300L, 700);
+        mapOA.put(600L, 1300);
 
-        assertTrue(map1.equals(map2));
+        mapJU.put(150L, 400);
+        mapJU.put(300L, 700);
+        mapJU.put(600L, 1300);
 
-        map2.put(1200, 2500L);
-        assertFalse(map1.equals(map2));
+        assertTrue(mapOA.equals(mapJU));
+
+        mapJU.put(1200L, 2500);
+        assertFalse(mapOA.equals(mapJU));
 
         try {
-            map1.put(1200, null);
+            mapOA.put(1200L, null);
             fail("Excepting " + UnsupportedOperationException.class
                     + " by inserting value = null");
         } catch (UnsupportedOperationException e) {
             // Ok, we waited for this
         }
-        assertFalse(map1.equals(map2));
+        assertFalse(mapOA.equals(mapJU));
 
-        map2.remove(Integer.valueOf(1200));
-        assertTrue(map1.equals(map2));
+        mapJU.remove(Long.valueOf(1200));
+        assertTrue(mapOA.equals(mapJU));
     }
 }
